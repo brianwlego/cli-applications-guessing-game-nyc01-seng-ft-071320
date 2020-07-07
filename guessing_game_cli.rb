d0ebb3e323e1949a7fc52b2ba1dfbc31dbdc4a56
Brian Lego <legobw@gmail.com>
2020-07-07 17:07:52 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,5 +3,11 @@
 def run_guessing_game
   comput_rand_num = rand(6)+1
   user_input = gets.chomp 
-  
+  if comput_rand_num == user_input
+    puts "You guessed the correct number!"
+  elsif comput_rand_num != user_input
+    puts "Sorry! The computer guessed #{comput_rand_num}"
+  elsif user_input == "exit"
+    puts "Goodbye!"
+  end
 end
