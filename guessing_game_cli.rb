17022d5d24838c263d5175e8c15a7342e8da47c8
Brian Lego <legobw@gmail.com>
2020-07-07 17:16:12 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -11,6 +11,6 @@ def run_guessing_game
     when "exit"
       puts "Goodbye!"
     else
-      puts "Sorry! The computer guessed #{comput_rand_num}"
+      puts "Sorry! The computer guessed #{comput_rand_num}."
   end
 end
