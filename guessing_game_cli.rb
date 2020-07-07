5da841a7dbc16e4d7ce63a415aca1791232f90bd
Brian Lego <legobw@gmail.com>
2020-07-07 17:14:51 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ require 'pry'
 def run_guessing_game
   comput_rand_num = rand(6)+1
   user_input = gets.chomp 
-  binding.pry 
+  #binding.pry 
   case user_input
     when comput_rand_num
       puts "You guessed the correct number!"
